9d59e86c9a7b8e288faa9312f89e31c5ca2085c8
Sergey Yakubov <sergey.yakubov@desy.de>
2017-04-13 17:11:28 +0200

    use authorization None

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,10 @@ export function make_auth(user, password, token) {
     if (token) {
         return token;
     }
+
+    if (password === 'none') {
+        return 'None ' + user;
+    }
     var tok = user + ':' + password;
     var hash = btoa(tok);
     return 'Basic ' + hash;
